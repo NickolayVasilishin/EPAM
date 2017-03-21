ecb9e41a1b717428ee3053f7baf21145b75ad34d
nickolay_vasilishin <nikolay_vasilishin@epam.com>
2017-03-21 01:30:41 +0100

    [tmp-2]

---
+++ Middle/webservice/src/test/java/com/epam/jmp/webservice/model/task1/Ticket.java
@@ -0,0 +1,62 @@
+
+package com.epam.jmp.webservice.model.task1;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for ticket complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ticket">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="state" type="{http://task1.model.webservice.jmp.epam.com/}state" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ticket", propOrder = {
+    "state"
+})
+public class Ticket {
+
+    @XmlSchemaType(name = "string")
+    protected State state;
+
+    /**
+     * Gets the value of the state property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link State }
+     *     
+     */
+    public State getState() {
+        return state;
+    }
+
+    /**
+     * Sets the value of the state property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link State }
+     *     
+     */
+    public void setState(State value) {
+        this.state = value;
+    }
+
+}
---
+++ Middle/webservice/src/test/java/com/epam/jmp/webservice/model/task1/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://task1.model.webservice.jmp.epam.com/")
+package com.epam.jmp.webservice.model.task1;
